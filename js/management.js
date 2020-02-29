bed38f9f04c8037231fea0b165d19af42b0a396d
panda928 <rjs595959@naver.com>
2020-02-29 22:57:19 +0900

    slide images

--- js/management.js
+++ js/management.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+	//event listener
 	function dragDown() {
 		var subMenu       = this.lastElementChild;
 		var parentWidth   = this.offsetWidth;
@@ -14,4 +15,31 @@ window.onload = function() {
 		slideMenuWrap[i].addEventListener("mouseover", dragDown);
 		slideMenuWrap[i].addEventListener("mouseout", dragUp);
 	}
+
+	//mainImage transition
+	function() {
+		var right=0;
+		function moveImage() {
+			if(right%900==0 && right!=1800) {
+				clearInterval(interval);
+				setTimeout(function() {
+					interval = setInterval(moveImage,10)},3000);
+				right+=2;
+				return;
+			}
+			if(right == 1800) {
+				right = 0;
+				clearInterval(interval);
+				setTimeout(function() {
+					interval = setInterval(moveImage,10)},3000);
+				return;
+			}
+
+			right+=2;
+			imageWrap.setAttribute("style","right:"+right+"px;");
+		}
+	}
+
+	var imageWrap = document.getElementsByClassName("mainImageWrapper")[0];
+	var interval = setInterval(moveImage,10);
 }
