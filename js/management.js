68267fe61acb000f8415ea7c263a3fa6584d9701
panda928 <rjs595959@naver.com>
2020-03-05 11:22:02 +0900

    use media query for resposive web

--- js/management.js
+++ js/management.js
@@ -1,42 +1,51 @@
 $(document).ready(function() {
-	//event listener
-	function dragDown() {
-		$(this).children('ul').css(
-			{"display":"block",
-			 "width"  : $(this).outerWidth()+"px"});
-	}
-	function dragUp() {
-		$(this).children('ul').css("display","none");
-	}
+	window.matchMedia("screen and (max-width:360px)");
+	if(window.matchMedia("screen and (max-width:360px)").matches) {
 
-	$('.slideMenuWrap').on("mouseover",dragDown).on("mouseout",dragUp);	
+	}
+	else if(window.matchMedia("screen and (max-width:1024px)").matches) {
 
-	//mainImage transition
-	var interval = setInterval(moveImage,10);
-	
-	var right=0;
-	var srcOrder = 0;
-	var moveContent = $('.mainImageWrapper').children();
-	function moveImage() {
-		if(right == 0) {
-			var imgs = [...moveContent.children()];
-			for(var i=0; i<imgs.length; i++) {
-				$(imgs[i]).attr("src","../images/siba"+((srcOrder+i)%6)+".jpg");
+	}
+	else {
+		//event listener
+		function dragDown() {
+			$(this).children('ul').css(
+				{"display":"block",
+				 "width"  : $(this).outerWidth()+"px"});
+		}
+		function dragUp() {
+			$(this).children('ul').css("display","none");
+		}
+
+		$('.slideMenuWrap').on("mouseover",dragDown).on("mouseout",dragUp);	
+
+		//mainImage transition
+		var interval = setInterval(moveImage,10);
+		
+		var right=0;
+		var srcOrder = 0;
+		var moveContent = $('.mainImageWrapper').children();
+		function moveImage() {
+			if(right == 0) {
+				var imgs = [...moveContent.children()];
+				for(var i=0; i<imgs.length; i++) {
+					$(imgs[i]).attr("src","../images/siba"+((srcOrder+i)%6)+".jpg");
+				}
+				srcOrder = (srcOrder+2)%6;
 			}
-			srcOrder = (srcOrder+2)%6;
-		}
-		if(right == 900) {
-			clearInterval(interval);
-			setTimeout(
-				function() {
-					interval = setInterval(moveImage,10);
-					return interval;
-				}, 3000);
+			if(right == 900) {
+				clearInterval(interval);
+				setTimeout(
+					function() {
+						interval = setInterval(moveImage,10);
+						return interval;
+					}, 3000);
+				$(moveContent).css("right", right+"px");
+				right = 0;
+				return;
+			}
 			$(moveContent).css("right", right+"px");
-			right = 0;
-			return;
+			right+=2;
 		}
-		$(moveContent).css("right", right+"px");
-		right+=2;
 	}
 });
